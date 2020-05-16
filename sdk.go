5e2ccbe6467c0e3b291e971fdbf5901c17cb97e8
Кирилл Кудинов <=>
2020-05-16 15:16:51 +0300

    correct handle error

--- sdk.go
+++ sdk.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -37,19 +38,34 @@ func NewDBClient(url string) *DBClient {
 }
 
 // SaveData RawData to PolySE Database
-func (d *DBClient) SaveData(collectionName string, data Documents) error {
+func (d *DBClient) SaveData(collectionName string, data Documents) (Documents, error) {
 	requestBody, err := json.Marshal(data)
 	if err != nil {
-		return err
+		return data, err
 	}
-	resp, err := http.Post(fmt.Sprintf("%s/api/%s/documents", d.url, collectionName), contentType, bytes.NewBuffer(requestBody))
+	client := http.Client{
+		Timeout: 20 * time.Second,
+	}
+	resp, err := client.Post(fmt.Sprintf("%s/api/%s/documents", d.url, collectionName), contentType, bytes.NewBuffer(requestBody))
 	defer resp.Body.Close()
 	if err != nil {
-		return err
-	} else if resp.StatusCode == http.StatusOK {
-		return nil
+		return data, err
+	} else if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusCreated {
+		return data, errors.New(http.StatusText(resp.StatusCode))
 	}
-	var answer string
-	_ = json.NewDecoder(resp.Body).Decode(&answer)
-	return errors.New(answer)
+	res := struct {
+		D       Documents `json:"documents"`
+		Message string    `json:"message"`
+	}{}
+	err = json.NewDecoder(resp.Body).Decode(&res)
+	if err != nil {
+		return data, err
+	}
+	if res.Message != "" {
+		if strings.Contains(res.Message, "200") == true || strings.Contains(res.Message, "201") == true {
+			return res.D, nil
+		}
+		return res.D, errors.New(res.Message)
+	}
+	return res.D, errors.New("Unexpected answer")
 }
